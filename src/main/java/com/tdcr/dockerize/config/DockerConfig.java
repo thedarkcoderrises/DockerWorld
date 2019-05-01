83fe52ba015f1e89505e92001a0462a251188e44
thedarkcoderrises <abhikrish88@gmail.com>
2019-05-01 10:24:18 +0530

    linking socat

--- src/main/java/com/tdcr/dockerize/config/DockerConfig.java
+++ src/main/java/com/tdcr/dockerize/config/DockerConfig.java
@@ -4,17 +4,22 @@ package com.tdcr.dockerize.config;
 import com.github.dockerjava.api.DockerClient;
 import com.github.dockerjava.core.DefaultDockerClientConfig;
 import com.github.dockerjava.core.DockerClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class DockerConfig {
 
+    @Value("${HOST_NAME:localhost}")
+    String host;
+
+
     @Bean
     DefaultDockerClientConfig defaultDockerClientConfig(){
         DefaultDockerClientConfig config
                 = DefaultDockerClientConfig.createDefaultConfigBuilder()
-                .withDockerHost("tcp://localhost:5555").build();
+                .withDockerHost("tcp://"+host+":5555").build();
         return config;
     }
 
